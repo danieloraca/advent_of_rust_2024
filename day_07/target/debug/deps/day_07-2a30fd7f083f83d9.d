40e3e9ea92402e8ab933bce542f51f25056b34ab
Dan <daniel.oraca@gmail.com>
2024-12-10 17:04:06 +0000

    cleanup

--- day_07/target/debug/deps/day_07-2a30fd7f083f83d9.d
+++
@@ -1,5 +0,0 @@
-/Users/danieloraca/Development/advent_of_rust_2024/day_07/target/debug/deps/libday_07-2a30fd7f083f83d9.rmeta: src/main.rs
-
-/Users/danieloraca/Development/advent_of_rust_2024/day_07/target/debug/deps/day_07-2a30fd7f083f83d9.d: src/main.rs
-
-src/main.rs:
--- day_07/target/debug/deps/day_07-a74e27634eb3d8e6.d
+++
@@ -1,5 +0,0 @@
-/Users/danieloraca/Development/advent_of_rust_2024/day_07/target/debug/deps/libday_07-a74e27634eb3d8e6.rmeta: src/main.rs
-
-/Users/danieloraca/Development/advent_of_rust_2024/day_07/target/debug/deps/day_07-a74e27634eb3d8e6.d: src/main.rs
-
-src/main.rs:
