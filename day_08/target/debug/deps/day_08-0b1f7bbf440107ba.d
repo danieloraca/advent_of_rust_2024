2c01f04d9417ab669d0d968816a162be21d39274
Dan <daniel.oraca@gmail.com>
2024-12-10 17:03:32 +0000

    day 10

---
+++ day_08/target/debug/deps/day_08-0b1f7bbf440107ba.d
@@ -0,0 +1,5 @@
+/Users/danieloraca/Development/advent_of_rust_2024/day_08/target/debug/deps/libday_08-0b1f7bbf440107ba.rmeta: src/main.rs
+
+/Users/danieloraca/Development/advent_of_rust_2024/day_08/target/debug/deps/day_08-0b1f7bbf440107ba.d: src/main.rs
+
+src/main.rs:
